60c13ba92b354f82d79658237521e990a88c5166
heshamabdalla <shamoh702@gmail.com>
2017-02-20 16:59:01 -0800

    style done - Hangman

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,22 @@
 
 $(function() {
   $(document).foundation();
+  introJs().start();
 
   var alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
-  var score = 0;
-  var letter;
-  var check;
-  var count = 0;
-  var life = 5;
-  var words;
-  var word = [];
-  var guess = [];
-  var index;
   var url = 'http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words';
+  var score = 0;
+  var counter = 0; // nextGame counter
+  var count = 0; // counter for Hangman Digram
+  var alphaLetter;
+  var correctWords; // holder for the full-word
+  var wordLetter = []; // array of letters
+  var secretWord = []; // array of "_"
+  var userName;
+  var level; // difficulty
+  var guesses = 5;
+  // var idx;
+
 
 
   var getWords = function(url, difficulty, minLength, maxLength) {
@@ -22,117 +26,151 @@ $(function() {
       data: {
         difficulty: difficulty,
         minLength: minLength,
-        maxLength: maxLength
+        maxLength: maxLength,
+        count: 1,
+        start: Math.floor(Math.random() * 100) + 0
       },
       success: function(data) {
-        data = data.replace(/\r?\n|\r/g, ' ').split(' ').splice(0, 1);
-        for(var i = 0; i < data.toString().split('').length; i++) {
-          word.push(data[i]);
-          console.log(data[data.split('')]])
+        if(data !== '') {
+
+        correctWords = data;
+        data = data.split('');
+        for(var i = 0; i < data.length; i++) {
+          wordLetter.push(data[i]);
+          console.log(wordLetter);
         }
-        getGuess(word)
+        getGuess(wordLetter);
+      } else {
+        console.error("DATA IS EMPTY! " + data);
+      }
       },
       error: function(err) {
-        console.log(err);
+        console.error(err);
       }
     });
-  }
+  };
 
-    var getGuess = function(word) {
-      for(var j = 0; j < word.length; j++) {
-        guess.push('_');
-        $('.words').append("<li class='guess"+j+"'>"+guess[j]+"</li>");
+  var getGuess = function(word) {
+    for(var j = 0; j < wordLetter.length; j++) {
+      secretWord.push('_');
+      $('.words').append("<li class='guess"+j+"'>"+secretWord[j]+"</li>");
+    }
+  };
+
+  //----- Get Customized Words-----\\
+  var getGameInfo = function(name, difficulty, minLength, maxLength) {
+    if( difficulty < 10 ) {
+      $('div.err').addClass('hide');
+      $('.name-field').hide();
+      $('.player').html("<h3>"+ name + "<small> Level " + difficulty +"<br>"+ " Max Length " + maxLength + "<br>"+ " Min Length " + minLength + " </small></h3>");
+      $('span.badge.success').html(guesses);
+      $('span.badge.secondary').html(score);
+      getWords(url, difficulty, minLength, maxLength);
+    } else {
+      $('div.err').removeClass('hide');
+    }
+  };
+
+  //----- Start Game -----\\
+  $('.play').click(function() {
+    userName = $('input.name').val();
+    level = $('.difficulty').val();
+    getGameInfo(
+      userName,
+      level,
+      $('.minLength').val(),
+      $('.maxLength').val()
+    );
+    $('#img').attr('src', './assets/images/first.png');
+  });
+
+  //----- Alphabet letters -----\\
+  $.each(alphabet, function(i, val) {
+    $('.button-group').append("<a class='button alphabet'>"+val+"</a>");
+  });
+
+  //---- Alphabet
+  $('a.alphabet').click(function() {
+    alphaLetter = $(this).text().toLowerCase();
+    // check if the letter in wordLetter array
+    for(var i = 0; i < wordLetter.length; i++) {
+      if ( wordLetter[i] === alphaLetter ) {
+        secretWord[i] = alphaLetter;
+        score += 48;
+        counter++;
+        $(this).addClass('disabled');
+        $('li.guess'+i).text(alphaLetter); // append the new letter
+        $('span.badge.secondary').html(score); // append the new score
+      }
+    }
+    var idx = (wordLetter.indexOf(alphaLetter));
+    if ( idx === - 1 ) {
+      guesses--;
+      count++;
+      console.error(idx, count);
+      $(this).addClass('disabled');
+      $('span.badge.success').html(guesses);
+    }
+
+    if( guesses < 1 ) {
+      $('#img').attr('src', './assets/images/hangman.gif');
+      $('#modal').html(
+        '<p class="lead">Your Score <span class="badge secondary">' + score +'</span></p>\
+        <p class="lead">The word was ' + correctWords.toUpperCase() +'</p>').foundation('open');
+         //module for the correct word and the score
+         newGame()
+      } else if (guesses === 5) {
+        $('#img').attr('src', './assets/images/first.png');
+      } else if (guesses === 4) {
+        $('#img').attr('src', './assets/images/second.png');
+      } else if (guesses === 3) {
+        $('#img').attr('src', './assets/images/third.png');
+      } else if (guesses === 2) {
+        $('#img').attr('src', './assets/images/fourth.png');
+      } else if (guesses === 1) {
+        $('#img').attr('src', './assets/images/fifth.png');
+      }
+      nextGame(counter, wordLetter.length); // check if player got the correct word
+  });
+
+    //----------- Next Game ------------\\
+    var nextGame = function(num, arrlen) {
+      if(num === arrlen) {
+        wordLetter = [];
+        secretWord = [];
+        alphaLetter = '';
+        counter = 0;
+        // count = 0;
+        $('.words').text('');
+        $('span.badge.secondary').html(score);
+        $('span.badge.success').html(guesses);
+
+        $('.button-group a').removeClass('disabled');
+        getGameInfo(userName, level, Math.floor(Math.random() * 6 ) + 1, Math.floor(Math.random() * 12 ) + 4);
       }
     };
 
-
-    var getUserInfo = function(name, difficulty, minLength, maxLength) {
-
-      if( name !== '' ) {
-        $('div.err').addClass('hide');
-        $('.name-field').hide();
-        $('.player').append("<h3>Welcome "+ name +"</h3>");
-        $('span.badge.success').html(life)
-        $('span.badge.secondary').html(score);
-        getWords(url, difficulty, minLength, maxLength);
-
-      } else {
-        $('div.err').removeClass('hide');
-      }
-
+    var newGame = function() {
+      secretWord = [];
+      guesses = 5;
+      count = 0;
+      score = 0;
+      alphaLetter = '';
+      wordLetter = [];
+      $('.words').text('');
+      $('span.badge.secondary').html(score);
+      $('span.badge.success').html(guesses);
+      $('#img').attr('src', './assets/images/first.png');
+      $('.button-group a').removeClass('disabled');
+      getGameInfo(userName, level, Math.floor(Math.random() * 6 ) + 0, Math.floor(Math.random() * 12 ) + 1);
     }
-
-    // Start the game
-    $('.play').click(function(){
-      getUserInfo(
-        $('.name').val(),
-        $('.difficulty').val(),
-        $('.minLength').val(),
-        $('.maxLength').val()
-      );
-
+    //-------- New Game ---------\\
+    $('#newGame').click(function() {
+      newGame();
     });
 
-    //-------------------------------------------
-    // populate alphabet letters
-    $.each(alphabet, function(i, val){
-      $('.button-group').append("<a class='button alphabet'>"+val+"</a>");
-    })
-    //-----------------------------------------------
 
 
-    $('a.alphabet').click(function(){ //TODO -> fix the counting problem
-      letter = $(this).text();
-      // $(this).addClass('disabled');
-      check = _.find(word, function(element, i) {
-        index = i;
-        return letter.toLowerCase() === element
-        // console.log(word.split('').splice(index, 1))
-        //returns true when found false otherwise
-      })
 
-
-      // if you found the letter in word
-      // keep letter and delete it from the word array
-      // check if the letter was found in the letter array
-      if(check) {
-        // if($('.guess'+index ).html() === "_" ) {
-        $( "li " ).each(function( idx ) {
-          // console.log( index + ": " + );
-          if( guess[idx] === "_") {
-            guess[idx] = letter
-            // console.log(word.indexOf(word[idx]))
-            // $( this ).text(letter)
-            score += 48; // increase the score
-            // .splice(index, 1, letter); // swap the guess with the correct letter
-
-            $('li.guess'+idx).text(letter); // append the new letter
-            $('span.badge.secondary').html(score); // append the new score
-            return false;
-          }
-        });
-
-
-      } else {
-        life--;
-        count++; // counter for swaping the images of hangman
-        $('span.badge.success').html(life);
-
-      }
-
-
-      // check if life is not 0 yet
-      if( life < 1 ) {
-        $('#img').attr('src', './assets/images/hangman.gif'); //swap the first img with the last one
-      } else if (count === 1) {
-        $('#img').attr('src', './assets/images/second.png')
-      } else if (count === 2) {
-        $('#img').attr('src', './assets/images/third.png')
-      } else if (count === 3) {
-        $('#img').attr('src', './assets/images/fourth.png')
-      } else if (count === 4) {
-        $('#img').attr('src', './assets/images/fifth.png')
-      }
-    })
-
-    });
+    console.log('%cREACH PILOT PROGRAM', "font-size: 40px; color: blue");
+  });
